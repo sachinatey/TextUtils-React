d336c41473ed6b6f708aeb8e3994c1ccb4de7169
Sachin <sachinatey@gmail.com>
2022-06-02 07:47:06 +0530

    Updates

---
+++ precache-manifest.4f2b9723126b2d73f103de3a5abb66d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a7625a9cb30378ba7bb0",
+    "url": "/TextUtils-React/static/css/main.c6e14a22.chunk.css"
+  },
+  {
+    "revision": "a7625a9cb30378ba7bb0",
+    "url": "/TextUtils-React/static/js/main.a7625a9c.chunk.js"
+  },
+  {
+    "revision": "cc360fb6938c4a946641",
+    "url": "/TextUtils-React/static/js/1.cc360fb6.chunk.js"
+  },
+  {
+    "revision": "1b9907dfd26f13e8151e",
+    "url": "/TextUtils-React/static/js/2.1b9907df.chunk.js"
+  },
+  {
+    "revision": "2499cddb344818b4039d",
+    "url": "/TextUtils-React/static/js/runtime~main.2499cddb.js"
+  },
+  {
+    "revision": "80661e6f3cf639c3bbb021ece5b25b1a",
+    "url": "/TextUtils-React/index.html"
+  }
+];
